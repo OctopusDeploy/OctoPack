6903262a5edc26a6e4c3df6558e2cf59ed7eecd1
Paul Stovell <paul@paulstovell.com>
2013-07-19 20:50:52 +1000

    Uninstall.ps1 is now specific about which project it uninstalls from

--- source/tools/Uninstall.ps1
+++ source/tools/Uninstall.ps1
@@ -6,7 +6,7 @@ function Uninstall-Targets ( $project )
 {
     Write-Host ("Removing OctoPack targets import from project: " + $project.Name)
 
-    $buildProject = Get-MSBuildProject
+    $buildProject = Get-MSBuildProject $project.Name
 
     $buildProject.Xml.Imports | Where-Object { $_.Project -match "OctoPack" } | foreach-object {     
         Write-Host ("Removing old import:      " + $_.Project)

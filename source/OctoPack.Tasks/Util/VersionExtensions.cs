f2612d74c2e6f1b9d2cfe2e7ca9775797c2da68e
Shane Gill <tothegills@gmail.com>
2016-09-09 11:36:57 +1000

    Rem string interpolation

--- source/OctoPack.Tasks/Util/VersionExtensions.cs
+++ source/OctoPack.Tasks/Util/VersionExtensions.cs
@@ -2,13 +2,13 @@
 
 public static class VersionExtensions
 {
-    const string SemanticVersionPattern = @"(?<semanticVersion>(\d+(\.\d+){0,3}" // Major Minor Patch
+    const string SemanticVersionPattern = @"^(?<semanticVersion>(\d+(\.\d+){0,3}" // Major Minor Patch
      + @"(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?)" // Pre-release identifiers
-     + @"(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?)"; // Build Metadata
+     + @"(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?)$"; // Build Metadata
 
     public static bool IsSemanticVersion(this string versionString)
     {
-        var match = Regex.Match(versionString, $@"^{SemanticVersionPattern}$");
+        var match = Regex.Match(versionString, SemanticVersionPattern);
 
         var versionMatch = match.Groups["semanticVersion"];
 

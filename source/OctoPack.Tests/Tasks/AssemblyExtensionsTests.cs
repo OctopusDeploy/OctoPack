bc594063e368d5d99b0b25c92379ac78530e3e78
David Beck <techiscool@gmail.com>
2017-10-26 12:35:42 -0700

    [Add] Test for Validation of Bug

--- source/OctoPack.Tests/Tasks/AssemblyExtensionsTests.cs
+++ source/OctoPack.Tests/Tasks/AssemblyExtensionsTests.cs
@@ -15,6 +15,14 @@ namespace OctoPack.Tests.Tasks
             return new FileInfo(assemblyPath).FullName;
         }
 
+        private string GetAssemblyFullExceptionPath(string name)
+        {
+            var currentAssemblyPath = Assembly.GetExecutingAssembly().FullLocalPath();
+            var configuration = new FileInfo(currentAssemblyPath).Directory.Name;
+            var assemblyPath = Path.Combine(currentAssemblyPath, "..", "..", "..", "..", name, "bin", configuration, "FileNotFoundException", $"{ name}.dll");
+            return new FileInfo(assemblyPath).FullName;
+        }
+
         [Test]
         public void AssertAssemblyVersion_WhereNoGitVersionProperty_ReturnsNull()
         {
@@ -27,6 +35,14 @@ namespace OctoPack.Tests.Tasks
         public void AssertAssemblyVersionGetsGitVersion()
         {
             var assemblyPath = GetAssemblyFullPath("OctoPack.Tests.SampleGitVersionAssembly");
+            var gitversion = AssemblyExtensions.GetNuGetVersionFromGitVersionInformation(assemblyPath);
+            Assert.That(gitversion, Is.EqualTo("1.1.1-tests"));
+        }
+
+        [Test]
+        public void AssertAssemblyFileNotFoundExceptionGetsGitVersion()
+        {
+            var assemblyPath = GetAssemblyFullExceptionPath("OctoPack.Tests.SampleGitVersionAssembly");
             var gitversion = AssemblyExtensions.GetNuGetVersionFromGitVersionInformation(assemblyPath);
             Assert.That(gitversion, Is.EqualTo("1.1.1-tests"));
         }

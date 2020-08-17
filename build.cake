c97ef1734649023050947e27bd2e7d31ff6879db
Shane <tothegills@gmail.com>
2020-08-17 12:45:09 +1000

    Build using cake instead of psake

---
+++ build.cake
@@ -0,0 +1,122 @@
+//////////////////////////////////////////////////////////////////////
+// TOOLS
+//////////////////////////////////////////////////////////////////////
+#tool "nuget:?package=GitVersion.CommandLine&version=4.0.0"
+#tool "nuget:?package=NUnit.Runners&version=2.6.4"
+#addin "Cake.FileHelpers&version=3.2.0"
+
+using Path = System.IO.Path;
+using IO = System.IO;
+using Cake.Common.Xml;
+//////////////////////////////////////////////////////////////////////
+// ARGUMENTS
+//////////////////////////////////////////////////////////////////////
+var target = Argument("target", "Default");
+var configuration = Argument("configuration", "Release");
+
+///////////////////////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES
+///////////////////////////////////////////////////////////////////////////////
+var buildDir = "./build/";
+var artifactsDir = "./artifacts/";
+
+GitVersion gitVersionInfo;
+string nugetVersion;
+
+
+///////////////////////////////////////////////////////////////////////////////
+// SETUP / TEARDOWN
+///////////////////////////////////////////////////////////////////////////////
+Setup(context =>
+{
+    gitVersionInfo = GitVersion(new GitVersionSettings {
+        OutputType = GitVersionOutput.Json
+    });
+
+    if(BuildSystem.IsRunningOnTeamCity)
+        BuildSystem.TeamCity.SetBuildNumber(gitVersionInfo.NuGetVersion);
+
+    nugetVersion = gitVersionInfo.NuGetVersion;
+
+    Information("Building OctoPack v{0}", nugetVersion);
+    Information("Informational Version {0}", gitVersionInfo.InformationalVersion);
+});
+
+Teardown(context =>
+{
+    Information("Finished running tasks.");
+});
+
+//////////////////////////////////////////////////////////////////////
+//  PRIVATE TASKS
+//////////////////////////////////////////////////////////////////////
+
+Task("Clean")
+    .Does(() =>
+{
+    CleanDirectory(buildDir);
+    CleanDirectory(artifactsDir);
+    CleanDirectories("./source/**/bin");
+    CleanDirectories("./source/**/obj");
+    CleanDirectories("./source/**/TestResults");
+});
+
+Task("Restore")
+    .IsDependentOn("Clean")
+    .Does(() => {
+        DotNetCoreRestore("source");
+    });
+
+
+Task("Build")
+    .IsDependentOn("Restore")
+    .IsDependentOn("Clean")
+    .Does(() => {
+        MSBuild("./source/OctoPack.sln", new MSBuildSettings
+        {
+            Configuration = configuration,
+            ArgumentCustomization = args => args.Append($"/p:Version={nugetVersion}")
+        });
+    });
+
+Task("Test")
+    .IsDependentOn("Build")
+    .WithCriteria(BuildSystem.IsLocalBuild)
+    .Does(() => {
+        var assemblies = GetFiles("./source/**/bin/**/*Tests.dll");
+        foreach (var assembly in assemblies) {
+                Information("Running test {0}", assembly.FullPath);
+                NUnit(assembly.FullPath);
+        }
+    });
+
+Task("Package")
+    .IsDependentOn("Test")
+    .Does(() => {
+        CreateDirectory(buildDir);
+        CreateDirectory(Path.Combine(buildDir, "content"));
+        CreateDirectory(Path.Combine(buildDir, "content", "net35"));
+        CreateDirectory(Path.Combine(buildDir, "content", "net40"));
+        CreateDirectory(Path.Combine(buildDir, "content", "netcore45"));
+        CreateDirectory(artifactsDir);
+
+        CopyDirectory($"./source/OctoPack.Tasks/bin/{configuration}", Path.Combine(buildDir, "build"));
+        CopyDirectory(@"./source/build", Path.Combine(buildDir, "build"));
+        CopyDirectory(@"./source/tools", Path.Combine(buildDir, "tools"));
+        CopyFileToDirectory(@".\source\OctoPack.nuspec", buildDir);
+        CopyFileToDirectory(@".\license.txt", buildDir);
+
+        NuGetPack(Path.Combine(buildDir, "OctoPack.nuspec"), new NuGetPackSettings {
+            Version = nugetVersion,
+            OutputDirectory = artifactsDir,
+            NoPackageAnalysis = true
+        });
+    });
+
+Task("Default")
+    .IsDependentOn("Package");
+
+//////////////////////////////////////////////////////////////////////
+// EXECUTION
+//////////////////////////////////////////////////////////////////////
+RunTarget(target);
--- build.ps1
+++ build.ps1
@@ -1,102 +1,195 @@
-## This is the OctoPack build script, written in PSAKE. Run with
-##
-##   Import-Module .\tools\psake\psake.psm1
-##   Invoke-psake .\build.ps1
-##
+##########################################################################
+# This is the Cake bootstrapper script for PowerShell.
+# This file was downloaded from https://github.com/cake-build/resources
+# Feel free to change this file to fit your needs.
+##########################################################################
 
-Framework "4.5.1"
+<#
 
-properties {
-    $configuration = "Release"
-    $nuget_path = ".\source\build\nuget.exe"
-    $gitversion_exe = ".\tools\GitVersion\GitVersion.exe"
-    $gitversion_remote_username = $ENV:GITVERSION_REMOTE_USERNAME
-    $gitversion_remote_password = $ENV:GITVERSION_REMOTE_PASSWORD
-}
+.SYNOPSIS
+This is a Powershell script to bootstrap a Cake build.
 
-task default -depends VerifyProperties, Package
+.DESCRIPTION
+This Powershell script will download NuGet if missing, restore NuGet tools (including Cake)
+and execute your Cake build script with the parameters you provide.
 
-task Clean {
-	write-host "Clean"
+.PARAMETER Script
+The build script to execute.
+.PARAMETER Target
+The build script target to run.
+.PARAMETER Configuration
+The build configuration to use.
+.PARAMETER Verbosity
+Specifies the amount of information to be displayed.
+.PARAMETER Experimental
+Tells Cake to use the latest Roslyn release.
+.PARAMETER WhatIf
+Performs a dry run of the build script.
+No tasks will be executed.
+.PARAMETER Mono
+Tells Cake to use the Mono scripting engine.
+.PARAMETER SkipToolPackageRestore
+Skips restoring of packages.
+.PARAMETER ScriptArgs
+Remaining arguments are added here.
 
-    $directories = ".\build"
+.LINK
+http://cakebuild.net
 
-    $directories | ForEach-Object {
-        Write-Output "Clean directory $_"
-        Remove-Item $_ -Force -Recurse -ErrorAction Ignore
-        New-Item -Path $_ -ItemType Directory -Force
+#>
+
+[CmdletBinding()]
+Param(
+    [string]$Script = "build.cake",
+    [string]$Target = "Default",
+    [ValidateSet("Release", "Debug")]
+    [string]$Configuration = "Release",
+    [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
+    [string]$Verbosity = "Verbose",
+    [switch]$Experimental = $true,
+    [Alias("DryRun","Noop")]
+    [switch]$WhatIf,
+    [switch]$Mono,
+    [switch]$SkipToolPackageRestore,
+    [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
+    [string[]]$ScriptArgs
+)
+
+[Reflection.Assembly]::LoadWithPartialName("System.Security") | Out-Null
+function MD5HashFile([string] $filePath)
+{
+    if ([string]::IsNullOrEmpty($filePath) -or !(Test-Path $filePath -PathType Leaf))
+    {
+        return $null
     }
-}
 
-task RunGitVersion {
-    exec {
-        # Log the current status, branch and latest log to help when things get confused...
-        Write-Host "Executing 'git status'"
-        & git status | Write-Host
-        Write-Host "Executing 'git log -n 1'"
-        & git log -n 1 | Write-Host
-
-        if ($gitversion_remote_username) {
-            $output = . $gitversion_exe /u "$gitversion_remote_username" /p "$gitversion_remote_password"
-        } else {
-            $output = . $gitversion_exe
+    [System.IO.Stream] $file = $null;
+    [System.Security.Cryptography.MD5] $md5 = $null;
+    try
+    {
+        $md5 = [System.Security.Cryptography.MD5]::Create()
+        $file = [System.IO.File]::OpenRead($filePath)
+        return [System.BitConverter]::ToString($md5.ComputeHash($file))
+    }
+    finally
+    {
+        if ($file -ne $null)
+        {
+            $file.Dispose()
         }
-
-        $formattedOutput = $output -join "`n"
-        Write-Host "Output from gitversion.exe"
-        Write-Host $formattedOutput
-
-        $versionInfo = $formattedOutput | ConvertFrom-Json
-        $script:package_version = $versionInfo.NuGetVersion
-        write-host "Package version:    $script:package_version"
-
-        if ($env:TEAMCITY_VERSION) {
-            TeamCity-SetBuildNumber $versionInfo.FullSemVer
-            write-host "TeamCity version: " + $versionInfo.FullSemVer
-        }
     }
 }
 
-task Build -depends Clean, RunGitVersion {
-	write-host "Build"
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 
-    exec {
-        msbuild .\source\OctoPack.sln /p:Configuration=$configuration /t:Rebuild
+Write-Host "Preparing to run build script..."
+if(!$PSScriptRoot){
+    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+
+$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+$NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
+$CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
+$NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
+$PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
+
+# Should we use mono?
+$UseMono = "";
+if($Mono.IsPresent) {
+    Write-Verbose -Message "Using the Mono based scripting engine."
+    $UseMono = "-mono"
+}
+
+# Should we use the new Roslyn?
+$UseExperimental = "";
+if($Experimental.IsPresent -and !($Mono.IsPresent)) {
+    Write-Verbose -Message "Using experimental version of Roslyn."
+    $UseExperimental = "-experimental"
+}
+
+# Is this a dry run?
+$UseDryRun = "";
+if($WhatIf.IsPresent) {
+    $UseDryRun = "-dryrun"
+}
+
+# Make sure tools folder exists
+if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
+    Write-Verbose -Message "Creating tools directory..."
+    New-Item -Path $TOOLS_DIR -Type directory | out-null
+}
+
+# Make sure that packages.config exist.
+if (!(Test-Path $PACKAGES_CONFIG)) {
+    Write-Verbose -Message "Downloading packages.config..."
+    try { (New-Object System.Net.WebClient).DownloadFile("http://cakebuild.net/download/bootstrapper/packages", $PACKAGES_CONFIG) } catch {
+        Throw "Could not download packages.config."
     }
 }
 
-task Package -depends Build {
-	write-host "Package"
-
-    mkdir .\build\content
-    mkdir .\build\content\net35
-    mkdir .\build\content\net40
-    mkdir .\build\content\netcore45
-    mkdir .\build\build
-    mkdir .\build\content\portable-net4+sl50+netcore45+wpa81+wp8
-    mkdir .\build\tools
-    dir -recurse .\source\OctoPack.Tasks\bin\$configuration | copy -destination build\build -Force
-    dir -recurse .\source\build | copy -destination build\build -Force
-    dir -recurse .\source\tools | copy -destination build\tools -Force
-    Copy-Item .\source\OctoPack.nuspec .\build
-    Copy-Item .\license.txt .\build
-
-    $base = (resolve-path "build")
-    write-host $base
-	exec {
-        & $nuget_path pack build\OctoPack.nuspec -basepath $base -outputdirectory $base -version $script:package_version -NoPackageAnalysis
+# Try find NuGet.exe in path if not exists
+if (!(Test-Path $NUGET_EXE)) {
+    Write-Verbose -Message "Trying to find nuget.exe in PATH..."
+    $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_) }
+    $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
+    if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
+        Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
+        $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
     }
 }
 
-## Helpers
-
-task VerifyProperties {
+# Try download NuGet.exe if not exists
+if (!(Test-Path $NUGET_EXE)) {
+    Write-Verbose -Message "Downloading NuGet.exe..."
+    try {
+        (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
+    } catch {
+        Throw "Could not download NuGet.exe."
+    }
 }
 
-Import-Module .\tools\psake\teamcity.psm1
+# Save nuget.exe path to environment to be available to child processed
+$ENV:NUGET_EXE = $NUGET_EXE
 
-TaskSetup {
-	if ($env:TEAMCITY_VERSION) {
-	    TeamCity-ReportBuildProgress "Running task $($psake.context.Peek().currentTaskName)"
-	}
+# Restore tools from NuGet?
+if(-Not $SkipToolPackageRestore.IsPresent) {
+    Push-Location
+    Set-Location $TOOLS_DIR
+
+    # Check for changes in packages.config and remove installed tools if true.
+    [string] $md5Hash = MD5HashFile($PACKAGES_CONFIG)
+    if((!(Test-Path $PACKAGES_CONFIG_MD5)) -Or
+      ($md5Hash -ne (Get-Content $PACKAGES_CONFIG_MD5 ))) {
+        Write-Verbose -Message "Missing or changed package.config hash..."
+        Remove-Item * -Recurse -Exclude packages.config,nuget.exe
+    }
+
+    Write-Verbose -Message "Restoring tools from NuGet..."
+    $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion -OutputDirectory `"$TOOLS_DIR`""
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "An error occured while restoring NuGet tools."
+    }
+    else
+    {
+        $md5Hash | Out-File $PACKAGES_CONFIG_MD5 -Encoding "ASCII"
+    }
+    Write-Verbose -Message ($NuGetOutput | out-string)
+    Pop-Location
 }
+
+# Make sure that Cake has been installed.
+if (!(Test-Path $CAKE_EXE)) {
+    Throw "Could not find Cake.exe at $CAKE_EXE"
+}
+
+# We added this so we can use dotnet tools
+# See https://www.gep13.co.uk/blog/introducing-cake.dotnettool.module
+Write-Host "Installing cake modules using the --bootstrap argument"
+&$CAKE_EXE --bootstrap
+
+# Start Cake
+Write-Host "Running build script..."
+Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
+exit $LASTEXITCODE
